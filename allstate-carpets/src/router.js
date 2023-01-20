f00102db58402fccc4616f0733775d054edfae38
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-20 13:46:10 -0500

    added skeleton for router file and added route for HomePage

--- allstate-carpets/src/router.js
+++ allstate-carpets/src/router.js
@@ -1,2 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Nav from './components/Nav.vue'
+import HomePage from './pages/HomePage.vue'
+
+const routes = [{ path: '/', component: HomePage, name: 'HomePage' }]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
