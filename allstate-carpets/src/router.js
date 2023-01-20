bac269e798713faf90d2ab31789e4a868df42f63
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-20 13:57:05 -0500

    added route for AboutUs

--- allstate-carpets/src/router.js
+++ allstate-carpets/src/router.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './pages/HomePage.vue'
+import AboutUs from './components/Aboutus.vue'
 
-const routes = [{ path: '/', component: HomePage, name: 'HomePage' }]
+const routes = [
+  { path: '/', component: HomePage, name: 'HomePage' },
+  { path: '/aboutus', component: AboutUs, name: 'AboutUs' }
+]
 
 const router = createRouter({
   history: createWebHistory(),
