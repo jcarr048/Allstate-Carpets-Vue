ec67c39ff738b087945497f2d8798e0cab75e044
Jeff Carroll <jcarr048@gmail.com>
2023-01-25 13:09:19 -0500

    Added filler text for about us

--- allstate-carpets/src/components/Aboutus.vue
+++ allstate-carpets/src/components/Aboutus.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="aboutus-container">
-    <h1 className="header">About Us</h1>
-    <p id="aboutus-text">Placeholder</p>
+  <div id="aboutus-container" className="mt-5">
+    <h1 className="font-bold">About Allstate Carpets</h1>
+    <p id="aboutus-text">In addition to providing the finest carpets, installation, and carpet care for individuals and homeowners, Tyson Floors also provides the same quality products and service for large commercial institutions. Our company has graced large projects such as schools, hospitals, nursing homes, airports, and government buildings.</p>
+
+    <p>Our carpet care facility rivals the finest in the United States because we know the products, we know how they should be cared for, and we know how your selection should perform given your unique environment. With this knowledge, as well as decades of experience, Tyson Floors is dedicated to creating the perfect environment for you, your home, your business, and your life.</p>
     <div id="staff-photo-container">
       <img className="about-picture" src="" alt="picture1" />
       <img className="about-picture" src="" alt="picture2" />
