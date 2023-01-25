30d2b8443df7324f3adb42f0d3b3a8cb2a13d70a
Jeff Carroll <jcarr048@gmail.com>
2023-01-25 13:08:47 -0500

    Added logo and tailwind classes

--- allstate-carpets/src/components/Nav.vue
+++ allstate-carpets/src/components/Nav.vue
@@ -1,5 +1,6 @@
 <template>
-  <nav className="flex flex-col md:flex-row">
+  <nav className="flex flex-col justify-center mx-auto my-auto md:flex-row">
+    <div> <img className="w-2/3 h-2/3" src="../assets/aclogo.jpeg" alt=""></div>
     <router-link to="/">Home</router-link>
     <router-link to="/aboutus">About Us</router-link>
   </nav>
